122a4f060ee88d7a2e3e6308850d67ff42fb9cc2
CJAX.Dev <jackson102890@gmail.com>
2024-09-24 15:44:04 -0600

    add content and create slideshow
    added slideshow in about me section and also made it transition every 5 seconds

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 // Variables
-
+const imgs= document.querySelectorAll(".about-img-wrapper img");
+let imgIndex = 0;
+let intervalId = null;
 // Functions
-
+function startSlideshow() {
+    if (imgs.length > 0) {
+        imgs[imgIndex].classList.add("show-img");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+function showSlide() {
+    if (imgIndex >= imgs.length) {
+        imgIndex = 0;
+    }
+    else if (imgIndex < 0) {
+        imgIndex = imgs.length - 1;
+    }
+    imgs.forEach(img => {
+        img.classList.remove("show-img");
+    });
+    imgs[imgIndex].classList.add("show-img");
+}
+function prevSlide() {
+    imgIndex++;
+    showSlide(imgIndex);
+}
+function nextSlide() {
+    imgIndex--;
+    showSlide(imgIndex);
+}
 // Events
+window.addEventListener("DOMContentLoaded", startSlideshow);
