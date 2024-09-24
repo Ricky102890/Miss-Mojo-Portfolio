5262b112b62dd6c9c51ff60404e91dee596cad68
CJAX.Dev <jackson102890@gmail.com>
2024-09-24 16:52:35 -0600

    add lots more features

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@
 const imgs= document.querySelectorAll(".about-img-wrapper img");
 let imgIndex = 0;
 let intervalId = null;
+const pubBtn = document.getElementById("pubMenu");
+const projectBtn = document.getElementById("projectMenu");
+const contactBtn = document.getElementById("contactMenu");
+const subBtn = document.getElementById("subMenu");
 // Functions
+// !! Needs work
+function showMegaMenu() {
+        }
 function startSlideshow() {
     if (imgs.length > 0) {
         imgs[imgIndex].classList.add("show-img");
@@ -31,3 +38,45 @@ function nextSlide() {
 }
 // Events
 window.addEventListener("DOMContentLoaded", startSlideshow);
+window.onscroll = () => {
+    const container1 = document.querySelectorAll(".main-animate1");
+    const container2 = document.querySelectorAll(".main-animate2");
+    const container3 = document.querySelectorAll(".main-animate3");
+    
+    container1.forEach(cont => {
+        let top = window.scrollY;
+        let contOffset = cont.offsetTop - 300;
+        let contHeight = cont.offsetHeight;
+
+        if (top < contOffset + contHeight) {
+            cont.classList.add("show-animate1");
+        }
+        else {
+            cont.classList.remove("show-animate1");
+        }
+    });
+    container2.forEach(cont => {
+        let top = window.scrollY;
+        let contOffset = cont.offsetTop - 300;
+        let contHeight = cont.offsetHeight;
+
+        if (top < contOffset + contHeight) {
+            cont.classList.add("show-animate2");
+        }
+        else {
+            cont.classList.remove("show-animate2");
+        }
+    });
+    container3.forEach(cont => {
+        let top = window.scrollY;
+        let contOffset = cont.offsetTop - 300;
+        let contHeight = cont.offsetHeight;
+
+        if (top < contOffset + contHeight) {
+            cont.classList.add("show-animate3");
+        }
+        else {
+            cont.classList.remove("show-animate3");
+        }
+    });
+}
